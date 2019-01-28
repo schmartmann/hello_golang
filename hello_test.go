4e865f2ca31f45291806f19a85f9ee15dc602291
schmartmann <stefanhartmann@gmail.com>
2019-01-28 14:39:43 -0500

    make test pass

--- hello_test.go
+++ hello_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestHello( t *testing.T ) {
   got := Hello()
-  want := "Hello, worldf"
+  want := "Hello, world"
 
   if got != want {
     t.Errorf( "got '%s' want '%s'", got, want )
